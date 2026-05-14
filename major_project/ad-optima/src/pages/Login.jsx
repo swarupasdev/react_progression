e73f99cb3cd29caf221f4839a84592ef9db44681
swarupasdev <santiswarupnayak1@gmail.com>
2026-05-14 23:29:55 +0530

    implement email/password login functionality in Login component

--- major_project/ad-optima/src/pages/Login.jsx
+++ major_project/ad-optima/src/pages/Login.jsx
@@ -1,15 +1,53 @@
 import { useContext } from "react"
-import { AuthContext } from "../context/AuthContext"
+import { useNavigate } from "react-router-dom"
+import { account } from "../config/Appwrite"
 
 function Login() {
-  const { login } = useContext(AuthContext)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const navigate = useNavigate()
+
+  async function handleLogic(e){
+        e.preventDefault()
+        try{
+          const session = await account.createEmailPasswordSession(
+            email,
+            password
+          )
+          console.log("Logged in:", session)
+
+          navigate("/dashboard")
+        } catch (error){
+            console.log(error)
+          }
+  
 
   return (
     <>
       <h1>Login Page</h1>
-      <button onClick={() => login("Swarup")}>Login</button>
+      
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Enter email"
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
+        />
+
+        <button type="submit">Login</button>
+
+
+      </form>
     </>
   )
+}
 }
 
 export default Login
