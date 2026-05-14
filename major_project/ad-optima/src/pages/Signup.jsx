99169621b78b270b6ee55583c1ef8e9e4adef2d3
swarupasdev <santiswarupnayak1@gmail.com>
2026-05-14 23:31:53 +0530

    implement signup functionality with form handling and user creation

--- major_project/ad-optima/src/pages/Signup.jsx
+++ major_project/ad-optima/src/pages/Signup.jsx
@@ -1,6 +1,65 @@
-function Signup(){
+import { useState } from "react"
+import { ID } from "appwrite"
+import { account } from "../config/Appwrite"
+import { useNavigate } from "react-router-dom"
 
-    return <h1>Signup Page</h1>
+function Signup() {
+  const [name,setName] = useState("")
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
 
+  const navigate = useNavigate()
+
+  async function handleSignup(e){
+    e.preventDefault()
+
+    try{
+      const response = await account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      )
+
+      console.log("User created:", response)
+
+      navigate("/login")
+
+    }catch(error){
+      console.log(error)
+    }
+  }
+
+  return (
+    <>
+      <h1>Signup Page</h1>
+
+      <form onSubmit={handleSignup}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e)=>setName(e.target.value)}
+        />
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}
+        />
+
+        <button type="submit">Signup</button>
+      </form>
+    </>
+  )
 }
+
 export default Signup
