5632b44201f9052a7fbfd9059456abcdab5f0198
swarupasdev <santiswarupnayak1@gmail.com>
2026-05-18 23:11:28 +0530

    feat(server): add campaign routes to the server

--- major_project/ad-optima/backend/server.js
+++ major_project/ad-optima/backend/server.js
@@ -1,12 +1,14 @@
 import express from "express"
 import cors from "cors"
 import authRoutes from "./routes/authRoutes.js"
+import campaignRoutes from "./routes/campaignRoutes.js"
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use("/api/auth", authRoutes)
+app.use("/api/campaigns", campaignRoutes)
 
 
 app.get("/", (req, res) => {
