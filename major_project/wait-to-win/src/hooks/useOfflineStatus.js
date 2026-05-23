5d847096e45a70524ec7d38f47d2c9dbf1612dba
swarupasdev <santiswarupnayak1@gmail.com>
2026-05-23 22:54:49 +0530

    chore: remove unused SVG and CSS files to clean up the project structure

---
+++ major_project/wait-to-win/src/hooks/useOfflineStatus.js
@@ -0,0 +1,18 @@
+import { useEffect, useState} from "react";
+ export default function useOfflineStatus(){
+    
+    const [isOffline, setOffline] = useState(!navigator.Online);
+
+    useEffect(()=>{
+        const goOffline = ()=> setIsOffline(true);
+        const goOnline = ()=> setIsOffline(false);
+
+        window.addEventListener("offline", goOffline);
+        window.addEventListener("online",goOnline);
+
+        return()=>{
+            window.removeEventListener("offline",goOffline)
+            window.removeEventListener("online",goOnline)
+        };
+    },[]);
+ }
