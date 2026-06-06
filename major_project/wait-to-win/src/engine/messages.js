bd1bb47611eb6ebd4fffe784f0057dec6e504b22
swarupasdev <santiswarupnayak1@gmail.com>
2026-06-06 23:56:38 +0530

    feat(messages): add initial set of game messages to enhance player engagement

---
+++ major_project/wait-to-win/src/engine/messages.js
@@ -0,0 +1,8 @@
+export const messages = [
+  "Patience creates value.",
+  "Most players claim too early.",
+  "Are you sure waiting is safe?",
+  "Greed destroys rewards.",
+  "Sometimes risk is rewarded.",
+  "You feel tempted already, right?",
+];
