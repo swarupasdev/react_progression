19400e19a9a0537c4b267ebc913a6988d8bfba4a
swarupasdev <santiswarupnayak1@gmail.com>
2026-06-07 12:08:44 +0530

    feat(Interview): add initial implementation of test function and example usage

---
+++ javascript_rev/Interview.js
@@ -0,0 +1,28 @@
+// function update(user){
+
+//     user.name = "Rahul";
+
+// }
+                                    //reference copied
+
+// let person = {
+//     name: "Swarup"
+// };
+
+
+// update(person);
+
+
+// console.log(person.name);
+
+function test(a){
+
+    a = 20;
+
+}
+                                    //local variable copied
+let x = 10;
+
+test(x);
+
+console.log(x);
