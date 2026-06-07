83583a066f113c277ac4b0440f6e4b1cf9992e6c
swarupasdev <santiswarupnayak1@gmail.com>
2026-06-07 12:28:25 +0530

    fix(Interview): update comments for clarity and correct initialization explanation for sayHi function

--- javascript_rev/Interview.js
+++ javascript_rev/Interview.js
@@ -59,3 +59,20 @@ function hello(){
     console.log("Hi");  //js already knew what hello() is during defining 
 
 }
+
+
+// sayHi();  you cannnot access it before initilization
+
+// let sayHi = function(){
+
+//     console.log("Hi");    //function expression not declaration 
+
+// };
+
+
+let sayHi = function(){
+
+    console.log("Hi");
+
+};
+sayHi()  //correct way you can access this sayHi( ) only after expressing it completely 
