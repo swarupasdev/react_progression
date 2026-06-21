9d44434d5c336821f2b4266512708040d9e08533
swarupasdev <santiswarupnayak1@gmail.com>
2026-06-21 22:05:39 +0530

    feat(Interview): add filtering and mapping functionality for employees, users, products, and tasks

--- javascript_rev/Interview.js
+++ javascript_rev/Interview.js
@@ -1,11 +1,42 @@
-// //# function:
+// // //# function:
+
+// // // function update(user){
+
+// // //     user.name = "Rahul"; //mutation
+
+// // // }
+// //                                     //reference copied
+
+// // // let person = {
+// // //     name: "Swarup"
+// // // };
+
+
+// // // update(person);
+
+
+// // // console.log(person.name);
+
+// // function test(a){
+
+// //     a = 20;
+
+// // }
+// //                                     //local variable copied
+// // let x = 10;
+
+// // test(x);
+
+// // console.log(x);
 
 // // function update(user){
 
-// //     user.name = "Rahul"; //mutation
+// //     user = {
+// //         name: "Rahul"   //changeing local reference only 
+// //     };
 
 // // }
-//                                     //reference copied
+
 
 // // let person = {
 // //     name: "Swarup"
@@ -17,167 +48,212 @@
 
 // // console.log(person.name);
 
-// function test(a){
-
-//     a = 20;
-
-// }
-//                                     //local variable copied
-// let x = 10;
-
-// test(x);
-
-// console.log(x);
-
-// function update(user){
-
-//     user = {
-//         name: "Rahul"   //changeing local reference only 
-//     };
-
-// }
 
 
-// let person = {
-//     name: "Swarup"
-// };
+// // //#hositing
+
+// // hello();   //hoisted the fucntion 
+
+// // function hello(){
+
+// //     console.log("Hi");  //js already knew what hello() is during defining 
+
+// // }
 
 
-// update(person);
+// // // sayHi();  you cannnot access it before initilizatio
 
+// // // let sayHi = function(){
 
-// console.log(person.name);
+// // //     console.log("Hi");    //function expression not declaration 
 
+// // // };
 
-
-// //#hositing
-
-// hello();   //hoisted the fucntion 
-
-// function hello(){
-
-//     console.log("Hi");  //js already knew what hello() is during defining 
-
-// }
-
-
-// // sayHi();  you cannnot access it before initilizatio
 
 // // let sayHi = function(){
 
-// //     console.log("Hi");    //function expression not declaration 
+// //     console.log("Hi");
 
 // // };
+// // sayHi()  //correct way you can access this sayHi( ) only after expressing it completely 
+
+// // function add(a,b){
+
+// //     return a+b;     //
+
+// // }
+
+// // // #callback
+// // function greet(name){
+
+// //     console.log("Hello " + name);
+
+// // }
+// //     //callback function 
+
+// // function process(callback){      // A callback is a function passed as an argument to another function, so it can be executed later. It allows us to control execution flow and is commonly used in asynchronous JavaScript.
+
+// //     callback("Swarup");
+
+// // }
 
 
-// let sayHi = function(){
+// // process(greet);
 
-//     console.log("Hi");
+// // //#closure
+// // function outer(){
 
-// };
-// sayHi()  //correct way you can access this sayHi( ) only after expressing it completely 
-
-// function add(a,b){
-
-//     return a+b;     //
-
-// }
-
-// // #callback
-// function greet(name){
-
-//     console.log("Hello " + name);
-
-// }
-//     //callback function 
-
-// function process(callback){      // A callback is a function passed as an argument to another function, so it can be executed later. It allows us to control execution flow and is commonly used in asynchronous JavaScript.
-
-//     callback("Swarup");
-
-// }
+// //     let count = 0;
 
 
-// process(greet);
+// //     return function inner(){
 
-// //#closure
-// function outer(){
+// //         count++;
 
-//     let count = 0;
+// //         console.log(count);
 
+// //     }
 
-//     return function inner(){
-
-//         count++;
-
-//         console.log(count);
-
-//     }
-
-// }
+// // }
 
 
-// let counter = outer();
+// // let counter = outer();
 
 
-// counter();
+// // counter();
 
-// counter();
+// // counter();
 
-// counter();
+// // counter();
 
 
-//# api response
-const employees = [
+// //# api response
+// const employees = [
 
- {id:1, name:"Swarup", active:true},
+//  {id:1, name:"Swarup", active:true},
 
- {id:2, name:"Rahul", active:false},
+//  {id:2, name:"Rahul", active:false},
 
- {id:3, name:"Amit", active:true}
+//  {id:3, name:"Amit", active:true}
 
-];
+// ];
 
-const result = employees 
-.filter((employee)=>employee.active===true)
-.map((employee)=>employee.name)
+// const result = employees 
+// .filter((employee)=>employee.active===true)
+// .map((employee)=>employee.name)
 
-console.log(result)
+// console.log(result)
 
-//Q2
+// //Q2
+// const users = [
+
+//  {id:1,name:"A"},
+
+//  {id:2,name:"B"},
+
+//  {id:1,name:"A"}
+
+// ];
+
+// const uniqueUsers = users.filter((user,index,array)=>{
+//     return index === array.findIndex((item)=>item.id===user.id)
+// })
+
+// console.log(uniqueUsers)
+
+// //Q3
+// const cart = [
+
+//  {product:"Laptop", price:50000},
+
+//  {product:"Mouse", price:1000},
+
+//  {product:"Keyboard", price:2000}
+
+// ];
+
+
+// const total = cart.reduce((sum,item)=>{   //reduce (accumulator,current value)
+
+//     return sum + item.price;
+
+// },0);
+
+
+// console.log(total);
+
+// # filter drill
+
+//Q1:   From the API response, return only users who are verified.
+
+//Api response
 const users = [
 
- {id:1,name:"A"},
+ {id:1, name:"Swarup", verified:true},
 
- {id:2,name:"B"},
+ {id:2, name:"Rahul", verified:false},
 
- {id:1,name:"A"}
+ {id:3, name:"Amit", verified:true}
 
 ];
+//my code
+const result1 = users.filter((item)=>{
+    return item.verified===true
+})
+console.log(result1)
 
-const uniqueUsers = users.filter((user,index,array)=>{
-    return index === array.findIndex((item)=>item.id===user.id)
+//Q2:   Return only products whose price is greater than 1000.
+//Api response
+const products = [
+
+ {id:1, name:"Phone", price:20000},
+
+ {id:2, name:"Cover", price:500},
+
+ {id:3, name:"Keyboard", price:1500}
+
+];
+//my code
+const result2 = products.filter((product)=>{
+
+    return product.price>1000
 })
 
-console.log(uniqueUsers)
+console.log(result2)
 
-//Q3
-const cart = [
+//Q3:   Remove all completed tasks and return only pending tasks.
+//Api response
+const tasks = [
 
- {product:"Laptop", price:50000},
+ {id:1, title:"Learn JS", status:"pending"},
 
- {product:"Mouse", price:1000},
+ {id:2, title:"Sleep", status:"completed"},
 
- {product:"Keyboard", price:2000}
+ {id:3, title:"React Practice", status:"pending"}
+
+];
+//my code
+const result3 = tasks.filter((task)=>{
+    return task.status==='pending'
+})
+console.log(result3)
+
+//Q4:   Search feature: return users whose name contains the searched text
+//Api response
+const newusers = [
+
+ {id:1, name:"Swarup"},
+
+ {id:2, name:"Rahul"},
+
+ {id:3, name:"Amit"}
 
 ];
 
 
-const total = cart.reduce((sum,item)=>{   //reduce (accumulator,current value)
+const search = "a";
+const result4 = newusers.filter((user)=>{
+    return user.name.toLowerCase().includes(search.toLowerCase())
+})
 
-    return sum + item.price;
-
-},0);
-
-
-console.log(total);
+console.log(result4)
